fb1a4e8d4abc7e134ab91582e5261860902f552b
aripbudiman <muhamadarifbudiman22@gmail.com>
2025-09-20 18:25:50 +0700

    feat: update pretier

--- backend/lib/services/impl/basic.module.service.ts
+++ backend/lib/services/impl/basic.module.service.ts
@@ -3,26 +3,26 @@ import { ModulePort } from '@lib/outbound/module.port'
 import { ModuleCreate, Modules, QuerySchema } from '@models/module.model'
 
 export class BasicModuleService implements ModuleSvc {
-  constructor(private repository: ModulePort) {}
+    constructor(private repository: ModulePort) {}
 
-  async createModule(data: ModuleCreate): Promise<Modules> {
-    return await this.repository.create(data)
-  }
+    async createModule(data: ModuleCreate): Promise<Modules> {
+        return await this.repository.create(data)
+    }
 
-  async updateModule(id: number, data: ModuleCreate): Promise<Modules> {
-    return await this.repository.update(id, data)
-  }
+    async updateModule(id: number, data: ModuleCreate): Promise<Modules> {
+        return await this.repository.update(id, data)
+    }
 
-  async deleteModule(id: number): Promise<string> {
-    await this.repository.delete(id)
-    return 'deleted'
-  }
+    async deleteModule(id: number): Promise<string> {
+        await this.repository.delete(id)
+        return 'deleted'
+    }
 
-  async getModule(id: number): Promise<Modules | null> {
-    return await this.repository.find(id)
-  }
+    async getModule(id: number): Promise<Modules | null> {
+        return await this.repository.find(id)
+    }
 
-  async getAllModules(query: QuerySchema): Promise<Modules[]> {
-    return await this.repository.findAll(query.limit, query.page)
-  }
+    async getAllModules(query: QuerySchema): Promise<Modules[]> {
+        return await this.repository.findAll(query.limit, query.page)
+    }
 }
